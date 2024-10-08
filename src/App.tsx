0b0b1d5b7eab90fe1e56a0e33c11c8b1400d9ad1
TurkeuBacon <99289161+TurkeuBacon@users.noreply.github.com>
2024-10-07 18:36:31 -0700

    Begin projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import TopBar from './components/TopBar/TopBar'
 import SideNav from './components/SideNav/SideNav'
+import Projects from './pages/Projects/Projects';
+import { Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <TopBar></TopBar>
-      <SideNav></SideNav>
-    </div>
+    <Router>
+      <div className='App'>
+        <TopBar></TopBar>
+        <SideNav></SideNav>
+        <div className='App-main-content-wrapper'>
+          <Routes>
+            <Route path="/" element={<Navigate to="/projects"/>} />
+            <Route path='/projects' Component={Projects}/>
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
