0d6faffb3b50cfe4da36d4635c84ba7d916a9368
Gabriel Bacon <gabriel@gabriel-laptop.attlocal.net>
2025-03-18 14:41:27 -0700

    Mobile warning message

--- src/App.tsx
+++ src/App.tsx
@@ -12,26 +12,42 @@ import WritingSample from './pages/WritingSample/WritingSample';
 import AutomatedPlaytestingPage from './pages/Projects/AutomatedPlaytesting/AutomatedPlaytesting';
 
 function App() {
-  return (
-    <Router>
-      <div className='App'>
-        <TopBar></TopBar>
-        <SideNav></SideNav>
-        <div className='App-main-content-wrapper'>
-          <Routes>
-            <Route path="/" element={<Navigate to="/projects"/>} />
-            <Route path='/projects' Component={Projects}/>
-            <Route path='/about' Component={About}/>
-            <Route path='/resume' Component={Resume}/>
-            <Route path='/writing_sample' Component={WritingSample}/>
-            <Route path='/projects/permanence_break' Component={PermanenceBreakPage}/>
-            <Route path='/projects/automated_playtesting' Component={AutomatedPlaytestingPage}/>
-            <Route path='/projects/permanence_break' Component={PermanenceBreakPage}/>
-          </Routes>
+  console.log(window.innerWidth);
+  if(window.innerWidth >= 1024) {
+    console.log("BIG");
+    return (
+      <Router>
+        <div className='App'>
+          <TopBar></TopBar>
+          <SideNav></SideNav>
+          <div className='App-main-content-wrapper'>
+            <Routes>
+              <Route path="/" element={<Navigate to="/projects"/>} />
+              <Route path='/projects' Component={Projects}/>
+              <Route path='/about' Component={About}/>
+              <Route path='/resume' Component={Resume}/>
+              <Route path='/writing_sample' Component={WritingSample}/>
+              <Route path='/projects/permanence_break' Component={PermanenceBreakPage}/>
+              <Route path='/projects/automated_playtesting' Component={AutomatedPlaytestingPage}/>
+              <Route path='/projects/permanence_break' Component={PermanenceBreakPage}/>
+            </Routes>
+          </div>
         </div>
+      </Router>
+    );
+  }
+  else {
+    console.log("Little");
+    return (
+      <div className="About-under-construction-wrapper">
+          <p className="About-under-construction-text">
+              Not yet built for mobile<br/>
+              Please visit this website on a computer<br/>
+              Or resize your browser and reload the page.
+          </p>
       </div>
-    </Router>
-  );
+    );
+  }
 }
 
 export default App;
